0e643e35e717997fed7cdb078656d0eea7c184b7
diangoreyes <ditzun@gmail.com>
2017-11-22 15:58:12 -0800

    added bootstrap, homes controller, contact, directions, home links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,6 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+//= require turbolinks
+//= require bootstrap-sprockets
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get 'contact_us' => 'home#contact_us'
+get 'directions' => 'home#directions'
+get 'index' => 'home#index'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'home#index'
 end

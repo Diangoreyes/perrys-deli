6d2c1b7b3d415e3f690fbeb08f970e3e8affe81e
diangoreyes <ditzun@gmail.com>
2017-11-27 09:43:33 -0800

    added salad model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,20 @@
 class HomeController < ApplicationController
   def index
+
   end
 
   def menu
+    @items = Item.all
+    @salads = Salad.all
   end
 
   def sandwiches
+	@items = Item.all
+
+  end
+
+  def salads 
+    @salads = Salad.all
   end
 
   def directions
